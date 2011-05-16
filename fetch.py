b0bd35881a492661603638310a97703c8258f7d6
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-16 18:26:27 +0200

    initial

---
+++ fetch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+# NOTE: I had to make a few fixes on mechanize and I also added images support.
+# So, in case this isn't upstream yet, you have to get mechanize from here:
+#   https://github.com/albertz/mechanize
+import mechanize
+
+br = mechanize.Browser()
+
+baseurl = "http://9-eyes.com/"
+
+def filename_for_url(url):
+	fn = url.replace(baseurl, "")
+	fn = fn.replace("/", "_")
+	if not fn.endswith(".jpg"): fn += ".jpg"
+	return fn
+
+def fetch(url):
+	fn = filename_for_url(url)
+	import urllib2
+	
+
+i = 1
+while True:
+	br.open(baseurl + "page/" + str(i))
+
+	c = 0
+	for img in br.images():
+		url = img.url
+		if baseurl not in url: continue
+		fetch(url)		
+		c += 1
+		
+	print "page", i, ", pictures:", c
+	if c == 0: break
+	
+	i += 1
